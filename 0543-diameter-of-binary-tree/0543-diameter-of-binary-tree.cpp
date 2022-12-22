c0c7e11df5d7c5d220d2d813c10cc7af1531e9a1
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2022-12-22 12:19:56 +0530

    Time: 15 ms (73.73%), Space: 20.3 MB (66.85%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,35 +1,45 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Brute Force
+
+// class Solution {
+// public:
+//     int maxi=0;
+//     void dfs(TreeNode* curr){
+//         if(!curr) return;
+//         int lh=height(curr->left);
+//         int rh=height(curr->right);
+//         maxi=max(maxi,lh+rh);
+//         dfs(curr->left);
+//         dfs(curr->right);
+//     }
+
+//     int height(TreeNode* node){
+//         if(!node) return 0;
+//         int lh=height(node->left);
+//         int rh=height(node->right);
+//         return 1+max(lh,rh);
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         dfs(root);
+//         return maxi;
+//     }
+// };
+
+// Optimal
+
 class Solution {
 public:
     int maxi=0;
-    void dfs(TreeNode* curr){
-        if(!curr) return;
-        int lh=height(curr->left);
-        int rh=height(curr->right);
+    int diameter(TreeNode* curr){
+        if(!curr) return 0;
+        int lh=diameter(curr->left);
+        int rh=diameter(curr->right);
         maxi=max(maxi,lh+rh);
-        dfs(curr->left);
-        dfs(curr->right);
-    }
-
-    int height(TreeNode* node){
-        if(!node) return 0;
-        int lh=height(node->left);
-        int rh=height(node->right);
         return 1+max(lh,rh);
     }
-    
+
     int diameterOfBinaryTree(TreeNode* root) {
-        dfs(root);
+        int temp= diameter(root);
         return maxi;
     }
 };
