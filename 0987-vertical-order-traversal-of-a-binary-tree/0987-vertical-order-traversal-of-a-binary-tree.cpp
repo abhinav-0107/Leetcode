9734edcfe4633fb145feb801e8ffe761a9897838
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2022-12-22 19:28:12 +0530

    Time: 7 ms (73.69%), Space: 13.6 MB (52.71%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,17 +14,14 @@ public:
         queue<pair<TreeNode*,pair<int,int>>>q;
         q.push({root,{0,0}});
         while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                auto t=q.front();
-                q.pop();
-                TreeNode* node=t.first;
-                int verti=t.second.first;
-                int level=t.second.second;
-                mp[verti][level].insert(node->val);
-                if(node->left) q.push({node->left,{verti-1,level+1}});
-                if(node->right) q.push({node->right,{verti+1,level+1}});
-            }
+            auto t=q.front();
+            q.pop();
+            TreeNode* node=t.first;
+            int verti=t.second.first;
+            int level=t.second.second;
+            mp[verti][level].insert(node->val);
+            if(node->left) q.push({node->left,{verti-1,level+1}});
+            if(node->right) q.push({node->right,{verti+1,level+1}});
         }
         vector<vector<int>> ans;
         for(auto i:mp){
