bb9740a289f96d9ff0343891be1a33617011d738
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-01-01 10:09:59 +0530

    Time: 1505 ms (7.14%), Space: 15.1 MB (14.29%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> closestPrimes(int left, int right) {
+        
+        // sieve of eratosthenes (Algo to find prime numbers less than n, here n=1e6+1)
+        vector<bool> p(1e6+1,true);
+        p[0]=p[1]=false;
+        for(int i=2;i*i<=1e6;i++){
+            for(int j=2;i*j<=1e6;j++) p[i*j]=false;
+        }
+        
+        // Comparing Adjancent primes pair forn finding the minimum pair
+        vector<int> ans;
+        vector<int> temp;
+        int diff=1e9;
+        for(int i=left;i<=right;i++){
+            if(p[i]){
+                temp.push_back(i);
+                if(temp.size()==2){
+                    if(temp[1]-temp[0]<diff){
+                        diff=temp[1]-temp[0];
+                        ans=temp;
+                    }
+                    temp.clear();
+                    temp.push_back(i);
+                }
+            }    
+        }
+        if(ans.size()==0) return {-1,-1};
+        return ans;
+    }
+};
