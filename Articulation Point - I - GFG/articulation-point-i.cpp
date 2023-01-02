30e6dec80e9f3924b35a9ea1815026a040137d51
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-01-02 19:57:00 +0530

    Added solution - LeetHub

---
+++ Articulation Point - I - GFG/articulation-point-i.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int timer =1;
+    void dfs(int node,int parent,vector<int> adj[],vector<int>& vis,vector<int>& tin,
+    vector<int>& low,vector<int>& mark){
+        vis[node]=1;
+        tin[node]=low[node]=timer;
+        timer++;
+        int child=0;
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            if(!vis[it]){
+                dfs(it,node,adj,vis,tin,low,mark);
+                low[node]=min(low[node],low[it]);
+                if(low[it] >= tin[node] && parent!= -1) mark[node]=1;
+                child++;
+            }
+            else low[node]=min(low[node],tin[it]);
+        }
+        if(child>1 && parent==-1) mark[node]=1;
+    }
+    
+    vector<int> articulationPoints(int n, vector<int>adj[]) {
+        vector<int> vis(n,0);
+        vector<int> tin(n),low(n);
+        vector<int> mark(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]) dfs(i,-1,adj,vis,tin,low,mark);
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(mark[i]) ans.push_back(i);
+        }
+        if(!ans.size()) return {-1};
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		vector<int> ans = obj.articulationPoints(V, adj);
+		for(auto i: ans)cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
