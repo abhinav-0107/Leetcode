2c86b85f1cd50613d9b77bc68d9d03075a4f7c06
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-01-03 19:56:54 +0530

    Update 0116-populating-next-right-pointers-in-each-node.cpp

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -16,6 +16,55 @@ public:
 };
 */
 
+
+
+
+
+// Method - 1 [Naive Method]
+
+// Logic -> By level Order Traversal
+
+class Solution {
+	public:
+		Node* connect(Node* root) {
+			if(!root) return NULL;
+
+			vector<vector<Node*>> lvl;
+			queue <Node*> q;
+			q.push(root);
+
+			while(!q.empty()){
+				int size = q.size();
+				vector <Node*> temp;
+
+				for(int i = 0 ; i < size ; i++){
+					Node* node = q.front();
+					q.pop();
+					if(node -> left) q.push(node -> left);
+					if(node -> right) q.push(node -> right);
+					temp.push_back(node);
+				}
+
+				temp.push_back(NULL);
+				lvl.push_back(temp);
+			} 
+
+			for(int i = 0 ; i < lvl.size() ; i++){
+				for(int j = 0 ; j < lvl[i].size() - 1 ; j++) lvl[i][j] -> next = lvl[i][j + 1];
+			}
+
+			return root;
+		}
+	};
+
+
+
+
+
+
+
+// Method - 2 [Optimized]
+
 class Solution {
 public:
     void dfs(Node* l,Node* r){
