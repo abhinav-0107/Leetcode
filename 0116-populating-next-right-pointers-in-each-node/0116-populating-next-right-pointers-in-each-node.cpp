a86c65d91defe1152d5f519ba2e6de7db05cd28c
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-01-03 19:51:53 +0530

    Time: 49 ms (19.11%), Space: 16.9 MB (58.43%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,27 +18,20 @@ public:
 
 class Solution {
 public:
+    void dfs(Node* l,Node* r){
+        if(!l && !r) return;
+        
+        l -> next = r;
+        r -> next = NULL;
+        
+        dfs(l -> left , l -> right);
+        dfs(l -> right , r -> left);
+        dfs(r -> left , r -> right);
+    }
+    
     Node* connect(Node* root) {
         if(!root) return NULL;
-        vector<vector<Node*>> lvl;
-        queue <Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector <Node*> temp;
-            for(int i = 0 ; i < size ; i++){
-                Node* node = q.front();
-                q.pop();
-                if(node -> left) q.push(node -> left);
-                if(node -> right) q.push(node -> right);
-                temp.push_back(node);
-            }
-            temp.push_back(NULL);
-            lvl.push_back(temp);
-        } 
-        for(int i = 0 ; i < lvl.size() ; i++){
-            for(int j = 0 ; j < lvl[i].size() - 1 ; j++) lvl[i][j] -> next = lvl[i][j + 1];
-        }
+        dfs(root -> left , root -> right);
         return root;
     }
 };
