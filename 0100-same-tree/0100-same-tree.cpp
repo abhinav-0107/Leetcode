4fe65eff89d3f8e3a8467eff18594fb70eb1bd7f
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-01-10 08:42:14 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (95.24%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,32 +1,10 @@
-//Brute Force
-
-// class Solution {
-// public:
-//     void preorder(TreeNode* Node, vector<int>&V){
-//         if(Node==NULL){
-//             V.push_back(INT_MIN);
-//             return ;
-//         }
-//         V.push_back(Node->val);
-//         preorder(Node->left,V);
-//         preorder(Node->right,V);
-//     }
-    
-//     bool isSameTree(TreeNode* p, TreeNode* q) {
-//         vector<int>v1,v2;
-//         preorder(p,v1);
-//         preorder(q,v2);
-//         return v1==v2;
-//     }
-// };
-
-
-//Optimal
-
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p || !q) return p==q;
+        if(!p || !q){
+            if(!p && !q) return true;
+            else return false;
+        }
         return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
