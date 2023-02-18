3aedaa36ac88af277be499116293ff07eb3f6781
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-02-18 17:25:39 +0530

    Time: 35 ms (12.13%), Space: 9.6 MB (29.94%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -1,3 +1,30 @@
+// Method -1
+// T->O(m n^2) && S->O(1)
+
+// class Solution {
+// public:
+//     int deleteGreatestValue(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         int temp = n,ans = 0;
+        
+//         while(temp--){
+//             int max_ele = 0;
+//             for(int i = 0 ; i < m ; i++){
+//                 auto max_ele_it = max_element(grid[i].begin(),grid[i].end());
+//                 max_ele = max(max_ele,*max_ele_it);
+//                 grid[i].erase(max_ele_it);
+//             }
+//             ans += max_ele;
+//         }     
+        
+//         return ans;
+//     }
+// };
+
+// Method -2
+// T->O(m nlogn) && S->O(1)
+
 class Solution {
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
@@ -8,9 +35,9 @@ public:
         while(temp--){
             int max_ele = 0;
             for(int i = 0 ; i < m ; i++){
-                auto max_ele_it = max_element(grid[i].begin(),grid[i].end());
-                max_ele = max(max_ele,*max_ele_it);
-                grid[i].erase(max_ele_it);
+                sort(grid[i].begin(),grid[i].end());
+                max_ele = max(max_ele,grid[i].back());
+                grid[i].pop_back();
             }
             ans += max_ele;
         }     
