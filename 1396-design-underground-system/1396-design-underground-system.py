1bac37446164c4bef845fe1d9d7d231815961590
Abhinav Singh <93193113+abhinav-0107@users.noreply.github.com>
2023-05-31 11:29:33 +0530

    Time: 256 ms (33.60%), Space: 26.6 MB (26.50%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -0,0 +1,28 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.journey = {}
+        self.history = {} # (startStation, endStation) => (allTime, allCount)
+
+    def checkIn(self, id: int, startStation: str, t: int) -> None:
+        self.journey[id] = (startStation, t)
+        
+
+    def checkOut(self, id: int, endStation: str, endTime: int) -> None:
+        startStation, startTime = self.journey.pop(id)
+        key = (startStation, endStation)
+        allTime, allCount = self.history.get(key, (0, 0))
+        self.history[key] = (allTime + (endTime - startTime), allCount + 1)
+        
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        key = (startStation, endStation)
+        allTime, allCount = self.history.get(key, (0, 0))
+        return allTime / allCount
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
